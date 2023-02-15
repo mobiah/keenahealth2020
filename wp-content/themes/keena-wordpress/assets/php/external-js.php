3e333b819f80140a8121c5f42c73d85ddea53e6d
michaelmobiah <michael@mobiah.com>
2023-02-15 13:54:59 -0700

    removing commented out GTM code

--- wp-content/themes/keena-wordpress/assets/php/external-js.php
+++ wp-content/themes/keena-wordpress/assets/php/external-js.php
@@ -4,7 +4,21 @@
 
 		?>
 <!-- Google Tag Manager -->
-<!-- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-W47MDPR');</script> -->
+<script>
+(function(w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js'
+    });
+    var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != 'dataLayer' ? '&l=' + l : '';
+    j.async = true;
+    j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+    f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'dataLayer', 'GTM-W47MDPR');
+</script>
 <!-- End Google Tag Manager -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
@@ -25,7 +39,8 @@
 
 		?>
 <!-- Google Tag Manager (noscript) -->
-<!-- <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W47MDPR" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>  -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W47MDPR" height="0" width="0"
+        style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) --><?php
 
 	} );
@@ -33,8 +48,23 @@
 	add_action( 'wp_footer', function() {
 
 		?>
-<!-- <script type="text/javascript">
-		    (function(e,t,o,n,p,r,i){e.visitorGlobalObjectAlias=n;e[e.visitorGlobalObjectAlias]=e[e.visitorGlobalObjectAlias]||function(){(e[e.visitorGlobalObjectAlias].q=e[e.visitorGlobalObjectAlias].q||[]).push(arguments)};e[e.visitorGlobalObjectAlias].l=(new Date).getTime();r=t.createElement("script");r.src=o;r.async=true;i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)})(window,document,"https://diffuser-cdn.app-us1.com/diffuser/diffuser.js","vgo");vgo('setAccount', '610640406');vgo('setTrackByDefault', true);vgo('process');</script> -->
+<script type="text/javascript">
+(function(e, t, o, n, p, r, i) {
+    e.visitorGlobalObjectAlias = n;
+    e[e.visitorGlobalObjectAlias] = e[e.visitorGlobalObjectAlias] || function() {
+        (e[e.visitorGlobalObjectAlias].q = e[e.visitorGlobalObjectAlias].q || []).push(arguments)
+    };
+    e[e.visitorGlobalObjectAlias].l = (new Date).getTime();
+    r = t.createElement("script");
+    r.src = o;
+    r.async = true;
+    i = t.getElementsByTagName("script")[0];
+    i.parentNode.insertBefore(r, i)
+})(window, document, "https://diffuser-cdn.app-us1.com/diffuser/diffuser.js", "vgo");
+vgo('setAccount', '610640406');
+vgo('setTrackByDefault', true);
+vgo('process');
+</script>
 
 <?php
 	} );
